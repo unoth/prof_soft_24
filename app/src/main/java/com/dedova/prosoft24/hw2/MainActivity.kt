6239eff1472c9dc5bfa0ad8cdbe8af63fe0172d0
unoth <dan005bk@gmail.com>
2024-07-13 20:08:05 +0300

    fix: add package hw2

--- app/src/main/java/com/dedova/prosoft24/hw2/MainActivity.kt
+++ app/src/main/java/com/dedova/prosoft24/hw2/MainActivity.kt
@@ -1,27 +1,33 @@
-package com.dedova.prosoft24
+package com.dedova.prosoft24.hw2
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dedova.prosoft24.R
 
-class SecondActivity : AppCompatActivity() {
+const val EXTRA_TEXT_VIEW = "text_view"
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_second)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val message = intent.getStringExtra(EXTRA_TEXT_VIEW)
-        val btn = findViewById<Button>(R.id.btn_notification)
+        val btn = findViewById<Button>(R.id.btn_to_second_activity)
+        val textView = getString(R.string.text_activity)
         btn.setOnClickListener {
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SecondActivity::class.java).apply {
+                putExtra(EXTRA_TEXT_VIEW, textView)
+            }
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/dedova/prosoft24/hw2/SecondActivity.kt
+++ app/src/main/java/com/dedova/prosoft24/hw2/SecondActivity.kt
@@ -1,32 +1,28 @@
-package com.dedova.prosoft24
+package com.dedova.prosoft24.hw2
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dedova.prosoft24.R
 
-const val EXTRA_TEXT_VIEW = "text_view"
-
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btn = findViewById<Button>(R.id.btn_to_second_activity)
-        val textView = getString(R.string.text_activity)
+        val message = intent.getStringExtra(EXTRA_TEXT_VIEW)
+        val btn = findViewById<Button>(R.id.btn_notification)
         btn.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java).apply {
-                putExtra(EXTRA_TEXT_VIEW, textView)
-            }
-            startActivity(intent)
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         }
     }
 }
